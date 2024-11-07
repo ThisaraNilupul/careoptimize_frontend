474688503112ae0a8d6e92191b40cc0c9f75c7eb
ThisaraNilupul <thisaranilupul99@gmail.com>
2024-11-07 12:55:10 +0530

    checkup history

--- src/pages/Patient/Patient-MedicalHistoryPage/PatientMedicalHistory.jsx
+++ src/pages/Patient/Patient-MedicalHistoryPage/PatientMedicalHistory.jsx
@@ -11,18 +11,25 @@ import axios from 'axios';
 import ButtonMain from '../../../components/BaseButton-component/Button';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import DownloadIcon from '@mui/icons-material/Download';
-import { green } from '@mui/material/colors';
+import { green , yellow } from '@mui/material/colors';
 
 const greenMain = green.A400;
 const greenHover = green.A700;
+const yellowMain = yellow.A400;
+const yellowHover = yellow.A700;
 
 function PatientMedicalHistory() {
   const patientId = localStorage.getItem('userId');
   const [allMedicalHistory, setAllMedicalHistory] = useState([]);
+  const [allCheckupsHistory, setAllCheckupsHistory] = useState([]);
   const [groupedTreatmentsHistory, setGroupedTreatmentsHistory] = useState({});
+  const [groupedCheckupsHistory, setGroupedCheckupsHistory] = useState({});
   const [expandedYears, setExpandedYears] = useState([]);
+  const [expandedCheckupsYears, setExpandedCheckupsYears] = useState([]);
   const [isOpenSelectedTreatment, setIsOpenSelectedTreatment] = useState(false);
   const [selectedTreatmenyHistory, setSelectedTreatmenyHistory] = useState(null);
+  const [isOpenSelectedCheckup, setIsOpenSelectedCheckup] = useState(false);
+  const [selectedCheckupHistory, setSelectedCheckupHistory] = useState(null);
 
   useEffect(() => {
     const getMedicalHistory = async () => {
@@ -40,7 +47,25 @@ function PatientMedicalHistory() {
     };
     
     getMedicalHistory();
-  }, [patientId]); 
+  }, [patientId]);
+  
+  useEffect(() => {
+    const getCheckupsHistory = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/patient/checkups-history/${patientId}`);
+
+        if (response.data && response.data.length > 0) {
+          setAllCheckupsHistory(response.data);
+        } else {
+          console.log("No Checkups history found for the patient.");
+        }
+      } catch (error) {
+          console.log("Error fetching Checkups history: ", error);
+      }      
+    }
+
+    getCheckupsHistory();
+  }, [patientId]);
 
 
   useEffect(() => {
@@ -49,6 +74,13 @@ function PatientMedicalHistory() {
       setGroupedTreatmentsHistory(grouped);
     }
   }, [allMedicalHistory]);
+
+  useEffect(() => {
+    if (allCheckupsHistory.length > 0) {
+      const grouped = groupCheckupsByYear(allCheckupsHistory);
+      setGroupedCheckupsHistory(grouped);
+    }
+  }, [allCheckupsHistory]);
 
   const groupTreatmentsByYear = (treatments) => {
     return treatments.reduce((acc, treatment) => {
@@ -63,11 +95,30 @@ function PatientMedicalHistory() {
     }, {});
   };
 
+  const groupCheckupsByYear = (checkups) => {
+    return checkups.reduce((acc, checkup) => {
+      const year = new Date(checkup.createdAt).getFullYear();
+
+      if (!acc[year]) {
+        acc[year] = [];
+      }
+
+      acc[year].push(checkup);
+      return acc;
+    }, {});
+  }
+
   const toggleYear = (year) => {
     setExpandedYears((prevExpanded) => 
         prevExpanded.includes(year) ? prevExpanded.filter((y) => y !== year) : [...prevExpanded, year]
     );
   };
+
+  const toggleCheckupsYear = (year) => {
+    setExpandedCheckupsYears((prevExpanded) => 
+      prevExpanded.includes(year) ? prevExpanded.filter((y) => y !== year) : [...prevExpanded, year]
+    );
+  }
 
   const setdateFormat = (date) => {
     const ndate = new Date(date).toLocaleDateString('en-US', {month: 'short', day: 'numeric'});
@@ -92,13 +143,22 @@ function PatientMedicalHistory() {
     setIsOpenSelectedTreatment(false);
   }
 
+  const handleOpenCheckup = (checkup) => {
+    setSelectedCheckupHistory(checkup);
+    setIsOpenSelectedCheckup(true);
+  }
+
+  const handleCloseCheckup = () => {
+    setIsOpenSelectedCheckup(false);
+  }
+
   return (
     <div className='medicalhistory'>
       <div className='filter'>
         <ButtonMain text="Apply Filter" height="28px" width="200px" variant="contained" color="#000000" bgColor={greenMain} bgHoverColor={greenHover} icon={<FilterAltIcon />}/>
       </div>
       <div className='medicalhistory-conten'>
-        <Card width="55vw" height="78vh">
+        <Card width="48vw" height="78vh">
           <div className='treatmenthistory-title'>Treatments History</div>
           <div className='treatmenthistory-container'>
             {Object.keys(groupedTreatmentsHistory).length === 0 ? (
@@ -130,8 +190,36 @@ function PatientMedicalHistory() {
             )}
           </div>
         </Card>
-        <Card width="24vw" height="78vh">
+        <Card width="31vw" height="78vh">
             <div className='treatmenthistory-title'>Medical Checkups History</div>
+            <div className='checkupshistory-container'>
+              {Object.keys(groupedCheckupsHistory).length === 0 ? (
+                <div className='checkupshistory-container-false'>No Checkups history available for this patient.</div>
+              ) : (
+                Object.keys(groupedCheckupsHistory).map((year) => (
+                  <div key={year} className='years-list'>
+                    <div className='Checkup-yeargroup' onClick={() => toggleCheckupsYear(year)}>
+                      <div className='i-year'>
+                      <TbActivityHeartbeat />{year} - Checkups
+                      </div>
+                      {expandedCheckupsYears.includes(year) ? <TfiAngleUp /> : <TfiAngleDown />}</div>
+                    <div>
+                      {expandedCheckupsYears.includes(year) && (
+                          <div className='checkup-list'>
+                              {groupedCheckupsHistory[year].map((checkup) => (
+                              <div key={checkup._id} className='checkup'>
+                                  <div className='checkup-name'><div className='t-tag'>Test :</div>{checkup.testName}</div>
+                                  <div className='checkup-date'><div className='t-tag'>Evaluated on :</div>{setdateFormat(checkup.createdAt)}</div>
+                                  <SubButton icon={<TbReportMedical />} height="29px" width="35px" onClick={() => handleOpenCheckup(checkup)}/>
+                              </div>
+                              ))}
+                          </div>
+                      )}
+                    </div>
+                  </div>
+                ))
+              )}
+            </div>
         </Card>
       </div>
       {isOpenSelectedTreatment && (
@@ -267,6 +355,62 @@ function PatientMedicalHistory() {
                     </div>
                 </div>
             </Card>
+        </div>
+      )}
+      {isOpenSelectedCheckup && (
+        <div className="popup">
+          <Card width="36vw" height="86vh">
+              <div className="popup-title">
+                    <div className='download-copy-title'>
+                        Checkup Info
+                    </div>
+                    <CloseButton icon={<LuX />} height="30px" width="30px" onClick={handleCloseCheckup} />
+              </div>
+              <div className='Checkupinfo-container'>
+              <ButtonMain text="Download PDF" height="28px" width="200px"  variant="contained" color="#000000" bgColor={yellowMain} bgHoverColor={yellowHover} icon={<DownloadIcon />}/>
+                <div className='checkup-info'>
+                  <div className='title'>Checkup Info</div>
+                  <div className='checkup-detalis'>
+                      <div className='detail-item'>
+                          <div className='checkup-name'>Checkup Id</div>
+                          <div className='ditem-value'>: {selectedCheckupHistory._id}</div>
+                      </div>
+                      <div className='detail-item'>
+                          <div className='checkup-name'>Test Name</div>
+                          <div className='ditem-value'>: {selectedCheckupHistory.testName}</div>
+                      </div>
+                      <div className='detail-item'>
+                          <div className='checkup-name'>Evaluated On</div>
+                          <div className='ditem-value'>: {setDateFormatwithyear(selectedCheckupHistory.createdAt)}</div>
+                      </div>
+                      <div className='detail-item'>
+                          <div className='checkup-name'>status</div>
+                          <div className='ditem-value'>: {selectedCheckupHistory.status}</div>
+                      </div>
+                      <div className='detail-item'>
+                          <div className='checkup-name'>Doctor's Feedback</div>
+                          <div className='ditem-value'>: {selectedCheckupHistory.feedback}</div>
+                      </div>
+                  </div>
+                </div>
+                <div className='doctor-info'>
+                  <div className='title'>Assigned By</div>
+                  <div className='adoctor-info'>
+                      <div className='ad-item'>Dr. {selectedCheckupHistory.assignByfirstName} {selectedCheckupHistory.assignBylastName}</div>
+                      <div className='ad-item'>{selectedTreatmenyHistory.treatmentInfo.doctorInfo.title}</div>
+                      <div className='ad-item'>{selectedTreatmenyHistory.treatmentInfo.doctorInfo.doctorPhone}</div>
+                      <div className='ad-item'>{selectedTreatmenyHistory.treatmentInfo.doctorInfo.doctorEmail}</div>
+                  </div>
+                  <div className='title'>Evaluated By</div>
+                  <div className='adoctor-info'>
+                      <div className='ad-item'>Dr. {selectedCheckupHistory.evaluateByfirstName} {selectedCheckupHistory.evaluateBylastName}</div>
+                      <div className='ad-item'>{selectedTreatmenyHistory.treatmentInfo.doctorInfo.title}</div>
+                      <div className='ad-item'>{selectedTreatmenyHistory.treatmentInfo.doctorInfo.doctorPhone}</div>
+                      <div className='ad-item'>{selectedTreatmenyHistory.treatmentInfo.doctorInfo.doctorEmail}</div>
+                  </div>
+                </div>
+              </div>
+          </Card>
         </div>
       )}
     </div>
